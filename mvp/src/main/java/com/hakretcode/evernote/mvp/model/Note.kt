8026ac2a68de7e17b581049f76912368ef79644a
hakretcode <hakretcode@gmail.com>
2021-09-09 23:54:54 -0300

    Initial commit

---
+++ mvp/src/main/java/com/hakretcode/evernote/mvp/model/Note.kt
@@ -0,0 +1,14 @@
+package com.hakretcode.evernote.mvc.model
+
+/**
+ *
+ * Setembro, 24 2019
+ * @author suporte@moonjava.com.br (Tiago Aguiar).
+ */
+data class Note(
+    var id: Int = 0,
+    var title: String? = null,
+    var desc: String? = null,
+    var date: String? = null,
+    var body: String? = null
+)
